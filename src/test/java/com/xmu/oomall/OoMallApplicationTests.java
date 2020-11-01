383f6befd1a42ab8b16fcca91e57b0b680fdb5f0
宋润涵 <sh963852741@126.com>
2020-11-01 16:57:19 +0800

    Initial commit

---
+++ src/test/java/com/xmu/oomall/OoMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmu.oomall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OoMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
