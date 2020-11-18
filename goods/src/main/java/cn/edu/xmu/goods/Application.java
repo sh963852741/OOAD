5ff0321d23b660eac8f8ba80af53bdbbf2f11b24
宋润涵 <sh963852741@126.com>
2020-11-18 22:43:41 +0800

    Update Application.java

--- goods/src/main/java/cn/edu/xmu/goods/Application.java
+++ goods/src/main/java/cn/edu/xmu/goods/Application.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"cn.edu.xmu.ooad","cn.edu.xmu.goods"})
-@MapperScan("cn.edu.xmu.ooad.mapper")
+@SpringBootApplication
+@MapperScan("cn.edu.xmu.goods.mapper")
 public class Application {
 
 	public static void main(String[] args) {
