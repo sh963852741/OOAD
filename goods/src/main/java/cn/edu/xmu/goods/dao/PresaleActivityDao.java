7f12f9ce39a1e926a10155f19c90eaa3e0135d3a
宋润涵 <sh963852741@126.com>
2020-11-25 16:23:04 +0800

    解决了Controller错误

--- goods/src/main/java/cn/edu/xmu/goods/dao/PresaleActivityDao.java
+++ goods/src/main/java/cn/edu/xmu/goods/dao/PresaleActivityDao.java
@@ -1,4 +1,10 @@
 package cn.edu.xmu.goods.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class PresaleActivityDao {
+
+    @Autowired
+    PresaleActivityDao presaleActivityDao;
+
 }
