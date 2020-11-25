2987763fb9447d7be82f6d1f93e88837391ae82a
宋润涵 <sh963852741@126.com>
2020-11-25 21:32:04 +0800

    重新生成了Mybatis并且写了Activity的Dao

--- goods/src/main/java/cn/edu/xmu/goods/dao/CouponActivityDao.java
+++ goods/src/main/java/cn/edu/xmu/goods/dao/CouponActivityDao.java
@@ -1,4 +1,76 @@
 package cn.edu.xmu.goods.dao;
 
+import cn.edu.xmu.goods.mapper.CouponActivityPoMapper;
+import cn.edu.xmu.goods.mapper.CouponSPUPoMapper;
+import cn.edu.xmu.goods.model.po.CouponActivityPo;
+import cn.edu.xmu.goods.model.po.CouponActivityPoExample;
+import cn.edu.xmu.goods.model.po.CouponSPUPo;
+import cn.edu.xmu.goods.model.po.CouponSPUPoExample;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CouponActivityDao {
+
+    @Autowired
+    CouponActivityPoMapper couponActivityPoMapper;
+    @Autowired
+    CouponSPUPoMapper couponSPUPoMapper;
+
+    List<CouponActivityPo> getActivitiesBySPUId(long id){
+        CouponSPUPoExample example = new CouponSPUPoExample();
+        CouponSPUPoExample.Criteria criteria = example.createCriteria();
+        criteria.andSpuIdEqualTo(id);
+
+        List<CouponSPUPo> couponSPUList = couponSPUPoMapper.selectByExample(example);
+        List<CouponActivityPo> activities = new ArrayList<>();
+        for(CouponSPUPo couponSPU: couponSPUList){
+            activities.add(couponActivityPoMapper.selectByPrimaryKey(couponSPU.getActivityId()));
+        }
+        return activities;
+    }
+
+    List<CouponActivityPo> getEffectiveActivities(int page, int pageSize, long shopId, int timeline){
+        PageHelper.startPage(page, pageSize);
+
+        CouponActivityPoExample example = new CouponActivityPoExample();
+        CouponActivityPoExample.Criteria criteria = example.createCriteria();
+
+        if (timeline == 0) {
+            /* 获取未开始的活动 */
+            criteria.andBeginTimeGreaterThan(LocalDateTime.now());
+        } else if (timeline == 1) {
+            /* 获取明天开始的活动 */
+            criteria.andBeginTimeGreaterThan(LocalDateTime.now());
+            criteria.andBeginTimeLessThan(LocalDateTime.now().plusDays(1));
+        } else if(timeline == 2) {
+            /* 获取正在进行中的活动 */
+            criteria.andBeginTimeLessThan(LocalDateTime.now());
+            criteria.andEndTimeGreaterThan(LocalDateTime.now());
+        } else if(timeline == 3) {
+            criteria.andEndTimeLessThan(LocalDateTime.now());
+        }
+        criteria.andShopIdEqualTo(shopId);
+
+        return couponActivityPoMapper.selectByExample(example);
+    }
+
+    boolean addActivity(CouponActivityPo po){
+        return couponActivityPoMapper.insert(po) == 1;
+    }
+
+    boolean delActivity(long id){
+        return couponActivityPoMapper.deleteByPrimaryKey(id) == 1;
+    }
+
+    boolean updateActivity(CouponActivityPo po) {
+        return couponActivityPoMapper.updateByPrimaryKey(po) == 1;
+    }
+
+    CouponActivityPo getActivityById(long id){
+        return couponActivityPoMapper.selectByPrimaryKey(id);
+    }
 }
--- goods/src/main/java/cn/edu/xmu/goods/dao/PresaleActivityDao.java
+++ goods/src/main/java/cn/edu/xmu/goods/dao/PresaleActivityDao.java
@@ -1,10 +1,61 @@
 package cn.edu.xmu.goods.dao;
 
+import cn.edu.xmu.goods.mapper.PresaleActivityPoMapper;
+import cn.edu.xmu.goods.model.po.PresaleActivityPo;
+import cn.edu.xmu.goods.model.po.PresaleActivityPoExample;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class PresaleActivityDao {
 
     @Autowired
-    PresaleActivityDao presaleActivityDao;
+    PresaleActivityPoMapper presaleActivityPoMapper;
 
+    List<PresaleActivityPo> getActivitiesBySPUId(long id){
+        PresaleActivityPoExample example = new PresaleActivityPoExample();
+        PresaleActivityPoExample.Criteria criteria = example.createCriteria();
+        criteria.andGoodsSpuIdEqualTo(id);
+
+        return presaleActivityPoMapper.selectByExample(example);
+    }
+
+    List<PresaleActivityPo> getEffectiveActivities(int page,int pageSize, long shopId, int timeline){
+        PageHelper.startPage(page, pageSize);
+
+        PresaleActivityPoExample example = new PresaleActivityPoExample();
+        PresaleActivityPoExample.Criteria criteria = example.createCriteria();
+
+        if (timeline == 0) {
+            /* 获取未开始的活动 */
+            criteria.andBeginTimeGreaterThan(LocalDateTime.now());
+        } else if (timeline == 1) {
+            /* 获取明天开始的活动 */
+            criteria.andBeginTimeGreaterThan(LocalDateTime.now());
+            criteria.andBeginTimeLessThan(LocalDateTime.now().plusDays(1));
+        } else if(timeline == 2) {
+            /* 获取正在进行中的活动 */
+            criteria.andBeginTimeLessThan(LocalDateTime.now());
+            criteria.andEndTimeGreaterThan(LocalDateTime.now());
+        } else if(timeline == 3) {
+            criteria.andEndTimeLessThan(LocalDateTime.now());
+        }
+        criteria.andShopIdEqualTo(shopId);
+
+        return presaleActivityPoMapper.selectByExample(example);
+    }
+
+    boolean addActivity(PresaleActivityPo po){
+        return presaleActivityPoMapper.insert(po) == 1;
+    }
+
+    boolean delActivity(long id){
+        return presaleActivityPoMapper.deleteByPrimaryKey(id) == 1;
+    }
+
+    boolean updateActivity(PresaleActivityPo po) {
+        return presaleActivityPoMapper.updateByPrimaryKey(po) == 1;
+    }
 }
