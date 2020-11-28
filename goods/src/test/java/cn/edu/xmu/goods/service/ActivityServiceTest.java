c52158cf387cbd21c82011d40809ab0ff40698d2
sh963852741 <sh963852741@126.com>
2020-11-28 09:53:09 +0800

    更新数据库初始化行为

--- goods/src/test/java/cn/edu/xmu/goods/service/ActivityServiceTest.java
+++ goods/src/test/java/cn/edu/xmu/goods/service/ActivityServiceTest.java
@@ -1,5 +1,6 @@
 package cn.edu.xmu.goods.service;
 
+import cn.edu.xmu.goods.model.vo.ActivityFinderVo;
 import cn.edu.xmu.ooad.util.ReturnObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,19 @@ class ActivityServiceTest {
     @Test
     void getPresaleActivityStatus() {
         ReturnObject ret = activityService.getPresaleActivityStatus();
-        assertEquals(200, ret.getCode());
+        assertEquals(0, ret.getCode());
     }
 
     @Test
     void getPresaleActivities() {
+        ActivityFinderVo vo = new ActivityFinderVo();
+        vo.setPage(1);
+        vo.setPageSize(10);
+        vo.setShopId(2L);
+        vo.setTimeline((byte)2);
+        vo.setSpuId(2L);
+        ReturnObject ret = activityService.getPresaleActivities(vo, true);
+        assertEquals(0, ret.getCode());
     }
 
     @Test
