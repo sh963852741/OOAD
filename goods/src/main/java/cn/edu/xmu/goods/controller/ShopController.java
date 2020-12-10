596e72f4479a2a14161f9c6905cddf7a23d18b21
yanglei <android9s@icloud.com>
2020-12-10 12:55:13 +0800

    判断卖家是否重复申请店铺

--- goods/src/main/java/cn/edu/xmu/goods/controller/ShopController.java
+++ goods/src/main/java/cn/edu/xmu/goods/controller/ShopController.java
@@ -68,9 +68,12 @@ public class ShopController {
         if (null != obj) {
             return obj;
         }
-
-        ReturnObject ret=shopService.newShop(shopvo);
-        return Common.decorateReturnObject(ret);
+        if(did==-1)
+        {
+            ReturnObject ret=shopService.newShop(shopvo);
+            return Common.decorateReturnObject(ret);
+        }
+        else return Common.decorateReturnObject(new ReturnObject(ResponseCode.APPLYAGAIN_ERROR));
     }
 
     /**
