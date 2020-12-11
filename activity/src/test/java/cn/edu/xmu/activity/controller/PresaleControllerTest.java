35d8469a36a2169ce90fe8341f88324daca9bd82
haomehaomebuhaoa <1138984922@qq.com>
2020-12-12 00:23:28 +0800

    徐悦提交 PresaleTest更新

--- activity/src/test/java/cn/edu/xmu/activity/controller/PresaleControllerTest.java
+++ activity/src/test/java/cn/edu/xmu/activity/controller/PresaleControllerTest.java
@@ -290,7 +290,7 @@ public class PresaleControllerTest {
 
         String request="{ \"name\": \"预售活动\", \"advancePayPrice\": 20, \"restPayPrice\": 3000, \"quantity\": 10, \"beginTime\": \"" + beginTime.toString()
                 + "\", \"payTime\": \"" + payTime.toString()
-                +"\",\"endTime\": \""+ endTime.toString() +"\"}";
+                +"\",\"endTime\": \""+ endTime.toString() +"\",\"state\":\"1\"}";
         ResultActions response = mvc.perform(post("/presale/shops/1/spus/290/presales")
                 .contentType("application/json;charset=UTF-8").content(request)
                 .header("authorization", shopToken));
@@ -318,6 +318,7 @@ public class PresaleControllerTest {
                 .andReturn().getResponse().getContentAsString();
     }
     @Test
+    //正常
     public void getPresaleState()throws Exception{
         ResultActions response = mvc.perform(get("/presale/presales/states")
                 .contentType("application/json;charset=UTF-8"));
@@ -330,8 +331,92 @@ public class PresaleControllerTest {
                 .andExpect(jsonPath("$.data[0].name").isString())
                 .andReturn().getResponse().getContentAsString();
     }
-
     @Test
+    //正常 用户根据timeline
+    public void getPresaleActivity1()throws Exception{
+        ResultActions response = mvc.perform(get("/presale/presales")
+                .queryParam("timeline", "1")
+                .queryParam("page", "1")
+                .queryParam("pageSize", "3")
+                .contentType("application/json;charset=UTF-8"));
+        String responseString = response.andExpect((status().isOk()))
+                .andExpect(jsonPath("$.errno").value(ResponseCode.OK.getCode()))
+                .andExpect(jsonPath("$.errmsg").value("成功"))
+                .andExpect(jsonPath("$.data.page").value(1))
+                .andExpect(jsonPath("$.data.pageSize").value(3))
+                .andExpect(jsonPath("$.data.list[0].name").isString())
+                .andExpect(jsonPath("$.data.list[0].BeginTime").isString())
+                .andExpect(jsonPath("$.data.list[0].payTime").isString())
+                .andExpect(jsonPath("$.data.list[0].endTime").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.id").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.name").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.skuSn").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.imageUrl").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.inventory").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.originalPrice").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.price").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.disable").isNumber())
+                .andReturn().getResponse().getContentAsString();
+
+    }
+    @Test
+    //正常 用户根据sku id
+    public void getPresaleActivity2()throws Exception{
+        ResultActions response = mvc.perform(get("/presale/presales")
+                .queryParam("sku id", "1")
+                .queryParam("page", "1")
+                .queryParam("pageSize", "3")
+                .contentType("application/json;charset=UTF-8"));
+        String responseString = response.andExpect((status().isOk()))
+                .andExpect(jsonPath("$.errno").value(ResponseCode.OK.getCode()))
+                .andExpect(jsonPath("$.errmsg").value("成功"))
+                .andExpect(jsonPath("$.data.page").value(1))
+                .andExpect(jsonPath("$.data.pageSize").value(3))
+                .andExpect(jsonPath("$.data.list[0].name").isString())
+                .andExpect(jsonPath("$.data.list[0].BeginTime").isString())
+                .andExpect(jsonPath("$.data.list[0].payTime").isString())
+                .andExpect(jsonPath("$.data.list[0].endTime").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.id").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.name").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.skuSn").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.imageUrl").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.inventory").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.originalPrice").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.price").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.disable").isNumber())
+                .andReturn().getResponse().getContentAsString();
+
+    }
+    @Test
+    //正常 用户根据shop id
+    public void getPresaleActivity3()throws Exception{
+        ResultActions response = mvc.perform(get("/presale/presales")
+                .queryParam("shop id", "1")
+                .queryParam("page", "1")
+                .queryParam("pageSize", "3")
+                .contentType("application/json;charset=UTF-8"));
+        String responseString = response.andExpect((status().isOk()))
+                .andExpect(jsonPath("$.errno").value(ResponseCode.OK.getCode()))
+                .andExpect(jsonPath("$.errmsg").value("成功"))
+                .andExpect(jsonPath("$.data.page").value(1))
+                .andExpect(jsonPath("$.data.pageSize").value(3))
+                .andExpect(jsonPath("$.data.list[0].name").isString())
+                .andExpect(jsonPath("$.data.list[0].BeginTime").isString())
+                .andExpect(jsonPath("$.data.list[0].payTime").isString())
+                .andExpect(jsonPath("$.data.list[0].endTime").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.id").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.name").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.skuSn").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.imageUrl").isString())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.inventory").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.originalPrice").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.price").isNumber())
+                .andExpect(jsonPath("$.data.list[0].goodsSpu.disable").isNumber())
+                .andReturn().getResponse().getContentAsString();
+
+    }
+    @Test
+    //正常
     public void modifyPresaleActivity1()throws Exception{
         LocalDateTime time = LocalDateTime.now();
         LocalDateTime beginTime = time.plusHours(2);
@@ -352,6 +437,7 @@ public class PresaleControllerTest {
                 .andReturn().getResponse().getContentAsString();
     }
     @Test
+    //无名称
     public void modifyPresaleActivity2()throws Exception{
         LocalDateTime time = LocalDateTime.now();
         LocalDateTime beginTime = time.plusHours(2);
@@ -367,14 +453,14 @@ public class PresaleControllerTest {
         String responseString = response.andExpect((status().isBadRequest()))
                 // .andExpect(content().contentType("application/json;charset=UTF-8"))
                 .andExpect(jsonPath("$.errno").value(ResponseCode.FIELD_NOTVALID.getCode()))
-                .andExpect(jsonPath("$.errmsg").value("must not be blank;"))
                 .andExpect(jsonPath("$.data").doesNotExist())
                 .andReturn().getResponse().getContentAsString();
     }
     @Test
+    //开始时间早于当前时间
     public void modifyPresaleActivity3()throws Exception{
         LocalDateTime time = LocalDateTime.now();
-        LocalDateTime beginTime = time.plusHours(2);
+        LocalDateTime beginTime = time.minusHours(2);
         LocalDateTime payTime = time.plusHours(3);
         LocalDateTime endTime = time.plusHours(4);
 
@@ -386,8 +472,87 @@ public class PresaleControllerTest {
                 .header("authorization", adminToken));
         String responseString = response.andExpect((status().isOk()))
                 // .andExpect(content().contentType("application/json;charset=UTF-8"))
-                .andExpect(jsonPath("$.errno").value(ResponseCode.OK.getCode()))
-                .andExpect(jsonPath("$.errmsg").value("成功"))
+                .andExpect(jsonPath("$.errno").value(ResponseCode.FIELD_NOTVALID.getCode()))
+                .andExpect(jsonPath("$.data").doesNotExist())
+                .andReturn().getResponse().getContentAsString();
+    }
+    @Test
+    //结束时间早于当前时间
+    public void modifyPresaleActivity4()throws Exception{
+        LocalDateTime time = LocalDateTime.now();
+        LocalDateTime beginTime = time.plusHours(2);
+        LocalDateTime payTime = time.plusHours(3);
+        LocalDateTime endTime = time.minusHours(4);
+
+        String request="{ \"name\": \"预售活动-改\", \"advancePayPrice\": 200, \"restPayPrice\": 300, \"quantity\": 110, \"beginTime\": \"" + beginTime.toString()
+                + "\", \"payTime\": \"" + payTime.toString()
+                +"\",\"endTime\": \""+ endTime.toString() +"\"}";
+        ResultActions response = mvc.perform(put("/presale/shops/0/presales/1")
+                .contentType("application/json;charset=UTF-8").content(request)
+                .header("authorization", adminToken));
+        String responseString = response.andExpect((status().isOk()))
+                // .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.errno").value(ResponseCode.FIELD_NOTVALID.getCode()))
+                .andExpect(jsonPath("$.data").doesNotExist())
+                .andReturn().getResponse().getContentAsString();
+    }
+    @Test
+    //支付尾款时间早于当前时间
+    public void modifyPresaleActivity5()throws Exception{
+        LocalDateTime time = LocalDateTime.now();
+        LocalDateTime beginTime = time.plusHours(2);
+        LocalDateTime payTime = time.minusHours(3);
+        LocalDateTime endTime = time.plusHours(4);
+
+        String request="{ \"name\": \"预售活动-改\", \"advancePayPrice\": 200, \"restPayPrice\": 300, \"quantity\": 110, \"beginTime\": \"" + beginTime.toString()
+                + "\", \"payTime\": \"" + payTime.toString()
+                +"\",\"endTime\": \""+ endTime.toString() +"\"}";
+        ResultActions response = mvc.perform(put("/presale/shops/0/presales/1")
+                .contentType("application/json;charset=UTF-8").content(request)
+                .header("authorization", adminToken));
+        String responseString = response.andExpect((status().isOk()))
+                // .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.errno").value(ResponseCode.FIELD_NOTVALID.getCode()))
+                .andExpect(jsonPath("$.data").doesNotExist())
+                .andReturn().getResponse().getContentAsString();
+    }
+    @Test
+    //尾款是负数
+    public void modifyPresaleActivity6()throws Exception{
+        LocalDateTime time = LocalDateTime.now();
+        LocalDateTime beginTime = time.plusHours(2);
+        LocalDateTime payTime = time.plusHours(3);
+        LocalDateTime endTime = time.plusHours(4);
+
+        String request="{ \"name\": \"\", \"advancePayPrice\": 200, \"restPayPrice\": -300, \"quantity\": 110, \"beginTime\": \"" + beginTime.toString()
+                + "\", \"payTime\": \"" + payTime.toString()
+                +"\",\"endTime\": \""+ endTime.toString() +"\"}";
+        ResultActions response = mvc.perform(put("/presale/shops/0/presales/1")
+                .contentType("application/json;charset=UTF-8").content(request)
+                .header("authorization", adminToken));
+        String responseString = response.andExpect((status().isBadRequest()))
+                // .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.errno").value(ResponseCode.FIELD_NOTVALID.getCode()))
+                .andExpect(jsonPath("$.data").doesNotExist())
+                .andReturn().getResponse().getContentAsString();
+    }
+    @Test
+    //开始时间早于当前时间
+    public void modifyPresaleActivity7()throws Exception{
+        LocalDateTime time = LocalDateTime.now();
+        LocalDateTime beginTime = time.minusHours(2);
+        LocalDateTime payTime = time.plusHours(3);
+        LocalDateTime endTime = time.plusHours(4);
+
+        String request="{ \"name\": \"预售活动-改\", \"advancePayPrice\": 200, \"restPayPrice\": 300, \"quantity\": 110, \"beginTime\": \"" + beginTime.toString()
+                + "\", \"payTime\": \"" + payTime.toString()
+                +"\",\"endTime\": \""+ endTime.toString() +"\"}";
+        ResultActions response = mvc.perform(put("/presale/shops/0/presales/1")
+                .contentType("application/json;charset=UTF-8").content(request)
+                .header("authorization", adminToken));
+        String responseString = response.andExpect((status().isOk()))
+                // .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.errno").value(ResponseCode.FIELD_NOTVALID.getCode()))
                 .andExpect(jsonPath("$.data").doesNotExist())
                 .andReturn().getResponse().getContentAsString();
     }
