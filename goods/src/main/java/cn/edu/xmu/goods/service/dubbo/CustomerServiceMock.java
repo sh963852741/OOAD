0a06c4141935560fcf14124d051b501a7169c2b9
99534 <995340474@qq.com>
2020-12-15 20:31:47 +0800

    合并修改bug

--- goods/src/main/java/cn/edu/xmu/goods/service/dubbo/CustomerServiceMock.java
+++ goods/src/main/java/cn/edu/xmu/goods/service/dubbo/CustomerServiceMock.java
@@ -1,22 +1,20 @@
 package cn.edu.xmu.goods.service.dubbo;
 
-import cn.edu.xmu.oomall.order.dto.AfterSaleDto;
-import cn.edu.xmu.oomall.order.dto.EffectiveShareDto;
-import cn.edu.xmu.oomall.order.dto.OrderItemDto;
-import cn.edu.xmu.oomall.order.service.IDubboOrderService;
-import cn.edu.xmu.other.dto.CustomerDto;
-import cn.edu.xmu.other.impl.ICustomerService;
+
+import cn.edu.xmu.oomall.other.dto.CustomerDTO;
+import cn.edu.xmu.oomall.other.impl.ICustomerService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceMock implements ICustomerService {
+
+
     @Override
-    public CustomerDto getCustomerById(long Id){
-        CustomerDto customerDto=new CustomerDto();
-        customerDto.setId(Id);
+    public CustomerDTO getCustomer(Long customerId) {
+        CustomerDTO customerDto=new CustomerDTO();
+        customerDto.setId(customerId);
         customerDto.setUserName("小沈");
         customerDto.setRealName("小小沈");
         return customerDto;
-
     }
 }
