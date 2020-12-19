2236ff4248176cbb48d91530e07eaf25c3605b15
99534 <995340474@qq.com>
2020-12-19 18:52:47 +0800

    新增dubbo接口 修改bug

--- goods/src/main/java/cn/edu/xmu/goods/utility/OtherAdapter.java
+++ goods/src/main/java/cn/edu/xmu/goods/utility/OtherAdapter.java
@@ -1,5 +1,8 @@
 package cn.edu.xmu.goods.utility;
 
+import cn.edu.xmu.privilegeservice.client.IUserService;
+import org.apache.dubbo.config.annotation.DubboReference;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,8 +11,10 @@ public class OtherAdapter {
     /**
      * @problem order 适配器
      */
- public static Map<String, Object> adapterSimpleUser(){
+ public static Map<String, Object> adapterSimpleUser(Long userId,String userName){
         Map<String, Object> ret = new HashMap<>();
+        ret.put("id",userId);
+        ret.put("userName",userName);
         return ret;
     }
 
